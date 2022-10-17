6eba458b578d8585f06b5dfeb924b6f5f1d0faf5
João Pedro <jp.siq.snts@gmail.com>
2022-10-16 23:40:17 -0300

    feat: login

---
+++ backend/src/main/java/dev/jpedrosnts/portifolio/config/security/WebSecurityConfigProd.java
@@ -0,0 +1,52 @@
+package dev.jpedrosnts.portifolio.config.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@Profile("prod")
+public class WebSecurityConfigProd extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    public WebSecurityConfigProd(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/mock/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and()
+                // .formLogin(form -> form
+                // .loginPage("/login")
+                // .defaultSuccessUrl("/usuario/pedido", true)
+                // .permitAll())
+                // .logout(logout -> {
+                // logout.logoutUrl("/logout").logoutSuccessUrl("/home");
+                // })
+                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource).usersByUsernameQuery(
+                        "SELECT nm_email, nm_senha, TRUE FROM tb_usuario WHERE nm_email=?")
+                .authoritiesByUsernameQuery(
+                        "SELECT nm_email, 'ROLE_DEFAULT' FROM tb_usuario WHERE nm_email=?")
+                .passwordEncoder(encoder);
+    }
+
+}
