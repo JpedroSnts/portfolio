528520f88f7340a0490b06e952c3bb17ed0cd632
João Pedro <jp.siq.snts@gmail.com>
2022-10-23 19:56:19 -0300

    test: configurações de testes

--- backend/src/main/java/dev/jpedrosnts/portifolio/config/security/WebSecurityConfigDev.java
+++ backend/src/main/java/dev/jpedrosnts/portifolio/config/security/WebSecurityConfigDev.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-@Profile("dev")
+@Profile({"dev","test"})
 public class WebSecurityConfigDev extends WebSecurityConfigurerAdapter {
 
     @Override
--- backend/src/test/java/dev/jpedrosnts/portifolio/PortifolioApplicationTests.java
+++ backend/src/test/java/dev/jpedrosnts/portifolio/PortifolioApplicationTests.java
@@ -1,13 +1,8 @@
 package dev.jpedrosnts.portifolio;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PortifolioApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
