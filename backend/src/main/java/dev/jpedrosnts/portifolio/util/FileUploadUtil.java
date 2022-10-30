5dd61ab59d44326f50a2a4799947b5092bb03680
João Pedro <jp.siq.snts@gmail.com>
2022-10-30 19:41:37 -0300

    feat: Upload de arquivos para a nuvem(blob azure)

---
+++ backend/src/main/java/dev/jpedrosnts/portifolio/util/FileUploadUtil.java
@@ -0,0 +1,60 @@
+package dev.jpedrosnts.portifolio.util;
+
+
+import com.azure.core.util.BinaryData;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+import com.azure.storage.blob.models.BlobHttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.net.URLConnection;
+
+@Component
+public class FileUploadUtil {
+
+
+    private final String connectionString;
+    private final String containerName;
+
+    @Autowired
+    public FileUploadUtil(
+            @Value("${azure.blob.container.name}") String containerName,
+            @Value("${azure.blob.connection.string}") String connectionString
+    ) {
+        this.connectionString = connectionString;
+        this.containerName = containerName;
+    }
+
+    public String upload(String fileName, byte[] image) {
+        BlobContainerClient container = new BlobContainerClientBuilder()
+                .containerName(containerName)
+                .connectionString(connectionString)
+                .buildClient();
+        BlobClient blob = container.getBlobClient(fileName);
+        BlobHttpHeaders headers = new BlobHttpHeaders();
+        headers.setContentType(getContentType(fileName));
+        blob.upload(BinaryData.fromBytes(image));
+        blob.setHttpHeaders(headers);
+        return blob.getBlobUrl();
+    }
+
+    private static String getContentType(String fileName) {
+        return URLConnection.getFileNameMap().getContentTypeFor(fileName);
+    }
+
+    public static boolean isImage(String fileName) {
+        try {
+            return getContentType(fileName).startsWith("image/") && !getContentType(fileName).contains("gif");
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public static String getExtension(String fileName) {
+        int index = fileName.lastIndexOf(".");
+        return fileName.substring(index);
+    }
+}
