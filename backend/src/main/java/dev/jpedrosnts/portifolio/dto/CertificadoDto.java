be0ea6c8aee668c92d34d5232c31625e66809033
João Pedro <jp.siq.snts@gmail.com>
2022-11-27 21:50:12 -0300

    feat: adicao de tecnologia no certificado

--- backend/src/main/java/dev/jpedrosnts/portifolio/dto/CertificadoDto.java
+++ backend/src/main/java/dev/jpedrosnts/portifolio/dto/CertificadoDto.java
@@ -1,6 +1,7 @@
 package dev.jpedrosnts.portifolio.dto;
 
 import dev.jpedrosnts.portifolio.model.Certificado;
+import dev.jpedrosnts.portifolio.model.Tecnologia;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,11 +18,13 @@ public class CertificadoDto {
     private UUID id;
     private String nome;
     private String certificado;
+    private Tecnologia tecnologia;
 
     public CertificadoDto(Certificado certificado) {
         this.id = certificado.getId();
         this.nome = certificado.getNome();
         this.certificado = certificado.getImagem();
+        this.tecnologia = certificado.getTecnologia();
     }
 
     public static List<CertificadoDto> toDto(List<Certificado> certificados) {
